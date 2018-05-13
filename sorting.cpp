9e9f46569f73818aa5ad3e62b54ff3a0ba7780b3
nikhilphulwari <nikhilphulwari@gmail.com>
2018-05-14 00:29:11 +0530

    updated sorting.cpp file

---
+++ sorting.cpp
@@ -0,0 +1,4 @@
+Algo – Repeatedly finding the minimum element and swapping.
+
+It basically determines the minimum (or maximum) of the list and swaps it with the element at the index where its supposed to be.
+The process is repeated such that the nth minimum (or maximum) element is swapped with the element at the n-1th index of the list. 
