28c3db6290e7a7d285d0da0ec75547ccf468fd4d
nikhilphulwari <nikhilphulwari@gmail.com>
2018-05-14 20:56:33 +0530

    updated sorting.cpp file

--- sorting.cpp
+++ sorting.cpp
@@ -1,4 +1,34 @@
-Algo – Repeatedly finding the minimum element and swapping.
-
-It basically determines the minimum (or maximum) of the list and swaps it with the element at the index where its supposed to be.
-The process is repeated such that the nth minimum (or maximum) element is swapped with the element at the n-1th index of the list. 
+#include <stdio.h>
+ 
+int main()
+{
+  int array[100], n, c, d, swap;
+ 
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+ 
+  printf("Enter %d integers\n", n);
+ 
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+ 
+  for (c = 0 ; c < ( n - 1 ); c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (array[d] > array[d+1]) /* For decreasing order use < */
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
+    }
+  }
+ 
+  printf("Sorted list in ascending order:\n");
+ 
+  for ( c = 0 ; c < n ; c++ )
+     printf("%d\n", array[c]);
+ 
+  return 0;
+}
